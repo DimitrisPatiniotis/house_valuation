a057bb49b7823ba3e87baa5f0f8f915be12f1fc9
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-02-14 15:02:17 +0200

    api-input-bug-fix

--- house_price_estimator_api/predictor_api/views.py
+++ house_price_estimator_api/predictor_api/views.py
@@ -2,6 +2,8 @@ from urllib import response
 from django.shortcuts import render
 import datetime
 import re
+import pandas as pd
+import numpy as np
 
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
@@ -13,6 +15,122 @@ from rest_framework import status
 import sys
 import pickle
 import time
+# Scalers
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+# Train Test Split
+from sklearn.model_selection import train_test_split
+
+dictionary =  {
+    'α' : 'a',
+    'Α' : 'a',
+    'ά' : 'a',
+    'Ά' : 'a',
+    'β' : 'b',
+    'Β' : 'b',
+    'γ' : 'g',
+    'Γ' : 'g',
+    'δ' : 'd',
+    'Δ' : 'd',
+    'ε' : 'e',
+    'έ' : 'e',
+    'Ε' : 'e',
+    'Έ' : 'e',
+    'ζ' : 'z',
+    'Ζ' : 'z',
+    'η' : 'h',
+    'ή' : 'h',
+    'Η' : 'h',
+    'Ή' : 'h',
+    'θ' : 'th',
+    'Θ' : 'th',
+    'ι' : 'i',
+    'ί' : 'i',
+    'ϊ' : 'i',
+    'Ι' : 'i',
+    'Ί' : 'i',
+    'κ' : 'k',
+    'Κ' : 'k',
+    'λ' : 'l',
+    'Λ' : 'l',
+    'μ' : 'm',
+    'Μ' : 'm',
+    'ν' : 'n',
+    'Ν' : 'n',
+    'ξ' : 'x',
+    'Ξ' : 'x',
+    'ο' : 'o',
+    'ό' : 'o',
+    'Ο' : 'o',
+    'Ό' : 'o',
+    'π' : 'p',
+    'Π' : 'p',
+    'ρ' : 'r',
+    'Ρ' : 'r',
+    'σ' : 's',
+    'Σ' : 's',
+    'ς' : 's',
+    'τ' : 't',
+    'Τ' : 't',
+    'υ' : 'u',
+    'ύ' : 'u',
+    'Υ' : 'u',
+    'Ύ' : 'u',
+    'φ' : 'f',
+    'Φ' : 'f',
+    'χ' : 'x',
+    'Χ' : 'x',
+    'ψ' : 'ps',
+    'Ψ' : 'ps',
+    'ω' : 'w',
+    'ώ' : 'w',
+    'Ω' : 'w',
+    'Ώ' : 'w',
+    ' ' : ' ',
+}
+
+def gr_to_en(grstr):
+    return ''.join([dictionary.get(i) for i in grstr])
+
+def reverse_year(year):
+    return  datetime.now().year - int(year)
+
+def label_encoding(columns_list, dataframe):
+    for col in columns_list:
+        dataframe[col] = dataframe[col].astype('category')
+        dataframe[col] = dataframe[col].cat.codes
+    return dataframe
+
+def one_hot_encoding(columns_list, dataframe):
+    for col in columns_list:
+        column = pd.get_dummies(dataframe[col])
+        dataframe = dataframe.join(column).drop(col, axis=1)
+    return dataframe
+
+def min_max_scaler(col, dataframe):
+    scaler = MinMaxScaler()
+    dataframe[col] = scaler.fit_transform(dataframe[col].array.reshape(-1,1))
+    return dataframe, scaler
+
+def standard_scaler(col, dataframe):
+    scaler = StandardScaler()
+    dataframe[col] = scaler.fit_transform(dataframe[col].array.reshape(-1,1))
+    return dataframe, scaler
+
+def split_data(data, test_size):
+    X = data.drop('price', axis=1)
+    y = data['price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return X_train, X_test, y_train, y_test
+
+def display_price_destr(price_col):
+    plt.xlabel('Price')
+    plt.ylabel('Number of samples')
+    plt.title('Price Distribution')
+    plt.plot()
+    plt.hist(price_col, facecolor = 'blue', edgecolor='peru', bins=15)
+    plt.show()
 
 def find_chars_until_space(str):
     find = re.compile(r"^[^ ]*")
@@ -114,7 +232,7 @@ def reverse_scale(scaler, price):
 def make_prediction(quer):
     start_timer = time.time()
     date = find_chars_until_space(str(datetime.datetime.now()))
-    csv_path = 'Datasets/house_data_{}.csv'.format(find_chars_until_space(str(datetime.datetime.now())))
+    csv_path = '../Datasets/house_data_{}.csv'.format(find_chars_until_space(str(datetime.datetime.now())))
     with open('../Models/model_{}.pickle'.format(date), 'rb') as f:
         unpickler = pickle.Unpickler(f)
         model = unpickler.load()
@@ -135,10 +253,15 @@ class call_model(APIView):
         if request.method == 'GET':
             
             # sentence is the query we want to get the prediction for
+            print('\n\n\n\n\n\n\n\n\n\n')
             params =  request.GET.get('list')
-            
+            params_list = params.split()
+            params_list = [str(params_list[0]) , str(params_list[1]), int(params_list[2]), int(params_list[3]), int(params_list[4]), int(params_list[5]), int(params_list[6])]
+            print(params_list)
             # predict method used to get the prediction
-            response = make_prediction(params)
-            
+            response = make_prediction(params_list)
+            response_dict = {
+                'estimated_price' : response
+            }
             # returning JSON response
-            return JsonResponse(response)
+            return JsonResponse(response_dict)
